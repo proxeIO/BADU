49b1218c2c79705745fedd965642cf2fcf3002a4
proxe <github@proxe.io>
2023-07-18 06:15:15 -0800

    Add basename

--- addon/path.py
+++ addon/path.py
@@ -4,6 +4,7 @@ from os import path, symlink
 sep = path.sep
 abspath = path.abspath
 dirname = path.dirname
+basename = path.basename
 
 scripts = join(bpy.utils.user_resource('SCRIPTS'), 'addons')
 
