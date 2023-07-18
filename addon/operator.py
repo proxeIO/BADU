1514d0c6d1921fbde1ca1b3c5b5acad9c3826a1b
proxe <github@proxe.io>
2023-07-17 23:22:33 -0800

    Add create_zip operator

--- addon/operator.py
+++ addon/operator.py
@@ -1,7 +1,32 @@
-# package
+from bpy.types import Operator
+from bpy.utils import register_class, unregister_class
+
 # symlink
 
+class ADU_OT_create_zip(Operator):
+    bl_idname = 'adu.create_zip'
+    bl_label = "Package Addon"
+    bl_description = "Create a zip archive of an addon"
+    bl_options = {'REGISTER', 'UNDO'}
+
+
+    @classmethod
+    def poll(cls, context):
+        if not context:
+            return False
+
+        packaging = context.window_manager.adu.packaging
+        default = packaging.__annotations__['source'].keywords['default']
+        source = packaging.source
+        return source not in {'', default} #TODO: use validate
+
+
+    def execute(self, context):
+        ...
+
+
 classes = (
+    ADU_OT_create_zip,
 )
 
 
@@ -11,6 +36,6 @@ def register():
 
 
 def unregister():
-    for cls in classes:
+    for cls in reversed(classes):
         unregister_class(cls)
 
